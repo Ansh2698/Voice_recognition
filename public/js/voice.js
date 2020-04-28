3c290407296e592f6768a262a29beda58d757ef7
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-04-28 16:52:37 +0530

    Grammar Added

--- public/js/voice.js
+++ public/js/voice.js
@@ -7,8 +7,13 @@ $(document).ready(function(){
             console.log("user has connected to this channel");
         })
     })
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    const recognition = new SpeechRecognition();
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var SpeechGrammarList=window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    var grammar="#JSGF V1.0";
+    var recognition = new SpeechRecognition();
+    var speechrecognitiongrammarList=new SpeechGrammarList();
+    speechrecognitiongrammarList.addFromString(grammar,1);
+    recognition.grammars = speechrecognitiongrammarList;
     recognition.lang = 'en-US';
     recognition.interimResults = false;
     document.querySelector('button').addEventListener('click', () => {
